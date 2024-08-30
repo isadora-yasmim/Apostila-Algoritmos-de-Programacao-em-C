b040e0f0d09d66d3fcff801e94490b51e7abc60b
Isadora Yasmim <164579225+isadora-yasmim@users.noreply.github.com>
2024-08-30 10:18:26 -0300

    exercícios cap 4

---
+++ Capítulo IV/7.2.c
@@ -0,0 +1,19 @@
+// E7.2-Distância entre dois pontos
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+   float x1,x2,y1,y2, X;
+    printf("\nDigite a coordenada x do primeiro ponto:");
+    scanf("%f", &x1);
+    printf("\nDigite a coordenada y do primeiro ponto:");
+    scanf("%f", &y1);
+    printf("\nDigite a coordenada x do segundo ponto:");
+    scanf("%f", &x2);
+    printf("\nDigite a coordenada y do primeiro ponto:");
+    scanf("%f", &y2);
+    X=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    printf("\nA DISTANCIA ENTRE A e B = %.2F", X);
+
+    return 0;
+}
