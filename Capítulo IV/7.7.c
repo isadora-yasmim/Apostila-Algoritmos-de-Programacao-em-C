4beedc16377849e45a54631c48f5fc46ca6ea6ec
Isadora Yasmim <164579225+isadora-yasmim@users.noreply.github.com>
2024-09-06 11:37:49 -0300

    correção 7.7

--- Capítulo IV/7.7.c
+++ Capítulo IV/7.7.c
@@ -1,10 +1,9 @@
-//corrigir
 // 7.7-Soma dos 3 menores valores
 /*30/8/24*/
 #include <stdio.h>
 
 int main() {
-    int v1,v2,v3,v4, m1,m2,m3,sm;
+    int v1,v2,v3,v4, maior, soma;
     printf("\nDigite o primeiro valor:");
     scanf("%d", &v1);
     printf("\nDigite o segundo valor:");
@@ -13,32 +12,20 @@ int main() {
     scanf("%d", &v3);
     printf("\nDigite o quarto valor:");
     scanf("%d", &v4);
-    if (v1>v2){
-        m1=v2;
-    }else{
-        m1=v1;
+    soma= v1+v2+v3+v4;
+    if (v1>=v2 && v1>= v3 && v1>=v4){
+        maior= v1;
     }
-    if (v1>v3){
-        m2=v3;
-    }else{
-        m2=v1;
+    if (v2>=v1 && v2>= v3 && v2>=v4){
+        maior= v2;
     }
-    if(v2>v3){
-        m3=v3;
-    }else{
-        m3=v2;
+    if (v3>=v2 && v3>= v1 && v3>=v4){
+        maior= v3;
     }
-    if (v4<m1){
-        m1=v4;
+    if (v4>=v2 && v4>= v3 && v4>=v1){
+        maior= v4;
     }
-    if (v4<m2){
-        m2=v4;
-    }
-    if (v4<m3){
-        m3=v4;
-    }
-    sm=m1+m2+m3;
-    printf("%d", sm);
+    printf("%d", soma- maior);
     
     return 0;
 }
