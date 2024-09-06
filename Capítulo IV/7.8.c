a25076abbaacd6ffde895f9fadfaf4ed940e76c0
Isadora Yasmim <164579225+isadora-yasmim@users.noreply.github.com>
2024-09-06 11:53:49 -0300

    update 7.8

--- Capítulo IV/7.8.c
+++ Capítulo IV/7.8.c
@@ -1,10 +1,5 @@
 // 7.8-Ordem
 /*30/8/24*/
-/******************************************************************************
-
-Saída errada, corrigir
-
-*******************************************************************************/
 #include <stdio.h>
 
 int main()
@@ -13,101 +8,70 @@ int main()
     int x,y,z,menor,meio,maior;
     
     printf("\nAdicione os valores a serem ordenados:");
+    scanf("\n%d%d%d", &a,&b,&c);
+    printf("\nAdicione os valores a ordem represnetada por ABC:");
     scanf("\n%d%d%d", &x,&y,&z);
-    while((x<0)||(y<0)||(z<0)||(x>100)||(y>100)||(z>100)){
-        printf("\nOs valores sao invalidos");  
-        printf("\nAdicione os valores a serem ordenados:");
-        scanf("\n%d%d%d", &x,&y,&z);
-    }
-    
-    printf("\nAdicione a ordem desejada simbolizada por A, B, C:");
-    scanf("\n%c%c%c", &a,&b,&c);
-    
-   // while((a!='A')||(b!='B')||(c!='C')||(a!='B')||(b!='C')||(c!='A')||(a!='C')||(b!='A')||(c!='B')){
-      //  printf("\nA ordem e invalida"); 
-       // printf("\nAdicione a ordem desejada simbolizada por A, B, C:");
-       // scanf("\n%c%c%c", &a,&b,&c);
-    //}
-    
-    if ((x<y)&&(x>z)){
-        meio=x;
-    }else{
-        if ((x>y)&&(x<z)){
-            meio=x;
+    //encontrar o menor elemento, o intermediario e o maior
+    if (a<= b && a <= c){
+        menor=a;
+        if(b<=c){
+            meio=b;
+            maior=c;
         }else{
-            if ((y>x)&&(y<z)){
-                meio=y;
-            }else{
-                 if ((y<x)&&(y>z)){
-                meio=y;
-            }else{
-                meio=z;
-            }
-            }
+            meio= c;
+            maior=b;
         }
-    }
-    
-    if (x>meio){
-        maior=x;
     }else{
-        if(x<meio){
-           menor=x;
+        if (b<= a && b <= c){
+        menor=b;
+        if(a<=c){
+            meio=a;
+            maior=c;
+        }else{
+            meio= c;
+            maior=a;
         }
-    }
-    
-    if(y>meio){
-        maior=y;
-    }else{
-        if(y<meio){
-            menor=y;
-            }
-    }
-    
-    if (z>meio){
-        maior=z;
-    }else{
-        if(z<meio){
-           menor=z;
         }
+        
+    }else{
+         if (c<= a && c <= b){
+        menor=c;
+        if(a<=b){
+            meio=a;
+            maior=b;
+        }else{
+            meio= b;
+            maior=a;
+        }
+     }
     }
+if (x=='A'){
+    printf("%d", menor);
+}
+if (x=='B'){
+    printf("%d", menor);
+}
+if (x=='C'){
+    printf("%d", menor);
+}
+if (y=='A'){
+    printf("%d", menor);
+}
+if (y=='B'){
+    printf("%d", menor);
+}
+if (y=='C'){
+    printf("%d", menor);
+}
+if (z=='A'){
+    printf("%d", menor);
+}
+if (z=='B'){
+    printf("%d", menor);
+}
+if (z=='C'){
+    printf("%d", menor);
+}
     
-    switch(a){
-        case1:'A';
-            a=menor;
-            break;
-        case2:'B';
-            a=meio;
-            break;
-        case3:'C';
-            a=maior;
-            break;
-    }
-    
-    switch(b){
-        case4:'A';
-            b=menor;
-            break;
-        case5:'B';
-            b=meio;
-            break;
-        case6:'C';
-            b=maior;
-            break;
-    } 
-    
-    switch(c){
-        case7:'A';
-            c=menor;
-            break;
-        case8:'B';
-            c=meio;
-            break;
-        case9:'C';
-            c=maior;
-            break;
-    }
-    
-    
-    printf("%d %d %d\n", a, b, c);
     return 0;
 }
