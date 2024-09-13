dc8880250d3b311e218fdf92ad18272e42cf3626
Isadora Yasmim <164579225+isadora-yasmim@users.noreply.github.com>
2024-09-13 09:39:32 -0300

    Update 7.9.c

--- Capítulo IV/7.9.c
+++ Capítulo IV/7.9.c
@@ -5,22 +5,23 @@
 int main()
 {
     int n, i;
-    float s, x;
+    double s, x;
     s=0.0;
     printf("\nDigite um número :");
     scanf("\n%d",&n);
-   /** while (n<1){
+    
+    while (n<1){
         printf("\nNumero invalido");
         printf("\nDigite um novo número :");
         scanf("\n%d",&n);
-    }**/
+    }
     
-    for(i=0; i<n; i++){
-        x=1/i;
+    for(i=1; i<=n; i++){
+        x=1.0/i;
         s+= x;
     }
     
-    printf("\n%f",s );
+    printf("\n%lf",s );
     
     
 
